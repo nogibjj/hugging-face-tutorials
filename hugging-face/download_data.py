1fc35a7f5378d3b53869ff9f61e33d1a558a77a5
Noah Gift <noah.gift@gmail.com>
2022-10-17 16:31:23 +0000

    adding harden

---
+++ hugging-face/download_data.py
@@ -0,0 +1,10 @@
+from datasets import load_dataset
+from pprint import pprint
+
+nba_dataset = load_dataset("noahgift/social-power-nba")
+
+print(f"Features: {nba_dataset['train'].features}\n")
+#print out the attributes of one example
+harden = nba_dataset['train'][1]
+#pprint out all features of harden
+pprint(harden)
