d63de9b850e130e818351bd3727659842603e8c4
Noah Gift <noah.gift@gmail.com>
2022-10-17 17:26:57 +0000

    adding gradio dataset example

---
+++ hugging-face/gradio-demos/dataset_gradio.py
@@ -0,0 +1,7 @@
+import gradio as gr
+
+# title and description are optional
+title = "Supersoaker Defective Product Prediction"
+description = "This model predicts Supersoaker production line failures. Drag and drop any slice from dataset or edit values as you wish in below dataframe component."
+
+gr.Interface.load("huggingface/scikit-learn/tabular-playground", title=title, description=description).launch()
